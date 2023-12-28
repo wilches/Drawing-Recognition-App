d8ca33467db980c42af9748cb3294f6dfe155baa
Juan Wilches <wilchesch@hotmail.com>
2023-12-27 20:55:41 -0500

    add a viewer for data directly on the webapp

--- common/utils.js
+++ common/utils.js
@@ -11,6 +11,25 @@ utils.printProgress = (count, max) => {
     process.stdout.write(count + "/" + max + " (" + percent + "%)");
 }
 
+
+/**
+ * Groups an array of objects by a specified key.
+ * @param {Array} objArray - The array of objects to be grouped.
+ * @param {string} key - The key to group the objects by.
+ * @returns {Object} - An object where the keys are unique values of the specified key and the values are arrays of objects with that key value.
+ */
+utils.groupBy = (objArray, key) => {
+    const groups = {};
+    for (let obj of objArray) {
+        const val = obj[key];
+        if (groups[val] == null){
+            groups[val] = [];
+        }
+        groups[val].push(obj);
+    }
+    return groups;
+}
+
 if (typeof module !== "undefined") {
     module.exports = utils;
   }
