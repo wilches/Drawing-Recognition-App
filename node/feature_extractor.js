d0c0c6282468ce6164aa2c448c0202698063a2a0
Juan Wilches <wilchesch@hotmail.com>
2024-01-13 22:18:17 -0500

    added more features to extract better conclusions

--- node/feature_extractor.js
+++ node/feature_extractor.js
@@ -11,10 +11,12 @@ for (const sample of samples) {
   const paths = JSON.parse(
     fs.readFileSync(constants.JSON_DIR + "/" + sample.id + ".json")
   );
-  sample.point = [featureFunctions.getPathCount(paths), featureFunctions.getPointCount(paths)];
+  const functions = featureFunctions.inUse.map(f => f.function)
+  sample.point = functions.map(f => f(paths));
+  
 }
 
-const featureNames = ["Path Count", "Point Count"];
+const featureNames = featureFunctions.inUse.map(f => f.name);
 
 fs.writeFileSync(
   constants.FEATURES,
