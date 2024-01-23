92c49008fcddc5c13186a29be7bcea51888cf830
Juan Wilches <wilchesch@hotmail.com>
2024-01-23 00:07:35 -0500

    added cross validation

--- node/feature_extractor.js
+++ node/feature_extractor.js
@@ -17,14 +17,31 @@ for (const sample of samples) {
   sample.point = functions.map(f => f(paths));  
 }
 
-const minMax = utils.normalizePoints(
-  samples.map(s => s.point)
-);
-
 const featureNames = featureFunctions.inUse.map(f => f.name);
 
-fs.writeFileSync(
-  constants.FEATURES,
+console.log("GENERATING SPLITS...");
+
+const trainingAmount = samples.length * 0.5;
+
+const training = [];
+const testing = [];
+
+for (let i = 0; i < samples.length; i++) {
+  if (i < trainingAmount) {
+    training.push(samples[i]);
+  } else {
+    testing.push(samples[i]);
+  }
+}
+
+const minMax = utils.normalizePoints(
+  training.map(s => s.point)
+);
+utils.normalizePoints(
+  testing.map(s => s.point), minMax
+);
+
+fs.writeFileSync(constants.FEATURES,
   JSON.stringify({
     featureNames,
     samples: samples.map(s => {
@@ -39,6 +56,40 @@ fs.writeFileSync(
 fs.writeFileSync(constants.FEATURES_JS,
     `const features =
     ${JSON.stringify({featureNames, samples})};`
+);
+
+fs.writeFileSync(constants.TRAINING,
+  JSON.stringify({
+    featureNames,
+    samples: training.map(s => {
+        return {
+            points: s.point,
+            label: s.label
+        };
+    })
+  })
+);
+
+fs.writeFileSync(constants.TRAINING_JS,
+    `const training =
+    ${JSON.stringify({featureNames, samples: training})};`
+);
+
+fs.writeFileSync(constants.TESTING,
+  JSON.stringify({
+    featureNames,
+    samples: testing.map(s => {
+        return {
+            points: s.point,
+            label: s.label
+        };
+    })
+  })
+);
+
+fs.writeFileSync(constants.TESTING_JS,
+    `const testing =
+    ${JSON.stringify({featureNames, samples: testing})};`
 );
 
 fs.writeFileSync(constants.MIN_MAX_JS,
